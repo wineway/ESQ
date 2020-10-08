812198e96b491e27689c2bee240eb25f441aa968
wineway <wangyuweihx@gmail.com>
2020-10-09 07:36:08 +0800

    add methods of ringbuffer

--- common/RingBuffer.h
+++ common/RingBuffer.h
@@ -7,21 +7,31 @@
 
 #include <cstddef>
 #include <cstring>
+#include <atomic>
 
 namespace esq {
     template<class T>
     class RingBuffer final {
     public:
         explicit RingBuffer(size_t size);
-        int append(T* data, size_t length);
+
+        int append(T *data, size_t length);
+
+        int commit(int index);
+
+        int getReadIndex() {
+            return readIndex;
+        }
+
+        int available();
 
         ~RingBuffer();
 
     private:
-        T* buf;
+        T *buf;
         size_t size;
-        int writeIndex;
-        int readIndex;
+        std::atomic<int> writeIndex;
+        std::atomic<int> readIndex;
 
         unsigned int retain();
     };
@@ -37,22 +47,41 @@ namespace esq {
         unsigned int ret = retain();
         unsigned int avl = ret > length ? length : ret;
         std::memcpy(&buf, data, avl);
-        writeIndex += avl;
-        return avl;
+        writeIndex.store((writeIndex + avl) % size, std::memory_order_relaxed);
+        return static_cast<int>(avl);
     }
 
     template<class T>
-    unsigned int RingBuffer<T>::retain() {
-        if (readIndex <= writeIndex) {
-            return writeIndex - readIndex;
+    /* must ensure access non-concurrent */unsigned int RingBuffer<T>::retain() {
+        int ri = readIndex.load(std::memory_order_consume);
+        int wi = writeIndex;
+        if (ri <= wi) {
+            return size - wi + ri;
         } else {
-            return writeIndex + size - readIndex;
+            return ri - wi;
         }
     }
 
     template<class T>
     RingBuffer<T>::~RingBuffer() {
-        delete [] this->buf;
+        delete[] this->buf;
+    }
+
+    template<class T>
+    int RingBuffer<T>::commit(int index) {
+        readIndex = index;
+        return 0;
+    }
+
+    template<class T>
+    int RingBuffer<T>::available() {
+        int wi = writeIndex.load(std::memory_order_consume);
+        int ri = readIndex;
+        if (ri >= wi) {
+            return size - ri + wi;
+        } else {
+            return wi - ri;
+        }
     }
 
 }
